6602f14cb946abfab850d874579026b0597052af
Rafael Lacerda Busatta <lacerdabusatta@gmail.com>
2023-09-06 11:38:37 -0300

    alteração do nome da janela na função principal

--- src/main.cpp
+++ src/main.cpp
@@ -405,7 +405,7 @@ int main(int argc, char *argv[])
     // Criamos uma janela do sistema operacional, com 800 colunas e 600 linhas
     // de pixels, e com título "INF01047 ...".
     GLFWwindow *window;
-    window = glfwCreateWindow(800, 600, "INF01047 - Seu Cartao - Seu Nome", NULL, NULL);
+    window = glfwCreateWindow(800, 600, "INF01047 - Fireworks App", NULL, NULL);
     if (!window)
     {
         glfwTerminate();
